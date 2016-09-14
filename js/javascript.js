a84dbcb368bba8049c5fb223e6dcc23e27c992e7
Icon <you@example.com>
2016-09-15 06:45:18 +0800

    figure doesnt go out of screen

--- js/javascript.js
+++ js/javascript.js
@@ -31,34 +31,65 @@ $(document).click(function() {
   console.log('mouse y: ' + event.screenY);
 })
 
+
 $(document).keydown(move);
 function move(e) {
-  if (GAMESTATUS === true) {
-    if (e.which === 37) {
+  // if (GAMESTATUS === true) {
+    if (e.which === 37 && hero.x1 > 40) {
         console.log('key left')
         $('#figure').animate({left: "-=125"}, 10)
+        console.log(hero.x1);
         // document.getElementById("figure").style('left') = '10'
         // hero.x -= 10
         // DetectCollision()
 
-    } else if (e.which === 39) {
+    } else if (e.which === 39 && hero.x1 < 283) {
         console.log('key right')
         $('#figure').animate({left: "+=125"}, 10)
-
+        // console.log(hero.x1);
         // hero.x += 10
         } else {
           // $('#figure').animate({bottom: "-=0"}, 5000)
           console.log("WRONG KEY! PRESS LEFT / RIGHT ONLY");
     }
-    if(detectCollision() === true) {
-      GAMESTATUS = false;
-    }
-    hero.x = $('#figure')[0].offsetLeft
-    hero.y = $('#figure')[0].offsetTop
+    // if(detectCollision() === true) {
+    //   GAMESTATUS = false;
+    // }
+    // hero.x = $('#figure')[0].offsetLeft
+    // hero.y = $('#figure')[0].offsetTop
+    //
+    // console.log(hero.x1, hero.y1, 'hero coordinates');
+  }
 
-    console.log(hero.x, hero.y, 'hero coordinates');
+
+function trackThings() {
+  // console.log('x: ' + hero.x + '. y: ' + hero.y)
+  var ob1 = $(".obstacle1").position();
+  obstacle1 = {x1:ob1.left, y1:ob1.top}
+  var ob2 = $(".obstacle2").position();
+  obstacle2 = {x1:ob2.left, y1:ob2.top}
+  var ob3 = $(".obstacle3").position();
+  var obstacle3 = {x1:ob3.left, y1:ob3.top}
+  // console.log('x1: ' + obstacle1.x1 + ' y1: ' + obstacle1.y1);
+  var heroo = $("#figure").position();
+  hero = {x1:heroo.left, y1:heroo.top};
+  // console.log(hero);
+}
+
+
+var score = 50
+var life = 3
+
+function simpleCollisionDetection() {
+  if (hero.x1 === obstacle1.x1 && hero.y1 > obstacle1.y1) {
+    score -= 1
+    console.log('score: ' + score);
   }
 }
+
+
+setInterval(trackThings, 100)
+setInterval(simpleCollisionDetection, 100)
 
 // var colliders_selector = "#figure";
 // var obstacles_selector = ".obstacle1";
@@ -66,11 +97,18 @@ function move(e) {
 // console.log(hits);
 
 
+// function generator() {
+//   // check if .obstacle1 & 2 or 2 & 3 or 1& 3 is around
+//   // if so, dont generate
+//   // if not, generate 1
+//   // if 0 found, generate 2
+//   if  ($('div.').length >
 
 
 
-setInterval(function() {generateObstacle1()}, 4000);
+setInterval(function() {generateObstacle1()}, 6000);
 function generateObstacle1 () {
+  $('.obstacle1').remove()
   $('#cloud-scroll').append('<div class="obstacle1"></div>')
   $('.obstacle1').animate({bottom: "+=600"}, Math.random()*10000)
 
@@ -81,7 +119,6 @@ setInterval(function() {generateObstacle2()}, 4000);
 function generateObstacle2 () {
   $('#cloud-scroll').append('<div class="obstacle2"></div>')
   $('.obstacle2').animate({bottom: "+=600"}, Math.random()*10000)
-
 
 }
 
@@ -101,23 +138,23 @@ function generateObstacle3 () {
 // $('.obstacle1').remove()
 
 
-function detectCollision() {
-  var ob1 = $(".obstacle1").position();
-  var ob2 = $(".obstacle2").position();
-  obstacle1 = {x1:ob1.left, y1:ob1.top, x2:ob1.left + 160, y2:ob1.top, x3:ob1.left, y3:ob1.top + 34, x4:ob1.left + 160, y4: ob1.top + 34}
-  obstacle2 = {x1:ob2.left, y1:ob2.top, x2:ob2.left + 160, y2:ob2.top, x3:ob2.left, y3:ob2.top + 34, x4:ob2.left + 160, y4: ob2.top + 34}
-  var blackOne = $("#figure").position();
-  hero = {x:blackOne.left, y:blackOne.top, w:blackOne.left + 70, h:blackOne.top - 44}
+// function detectCollision() {
+
+  // var ob2 = $(".obstacle2").position();
+
+
+  // obstacle2 = {x1:ob2.left, y1:ob2.top, x2:ob2.left + 160, y2:ob2.top, x3:ob2.left, y3:ob2.top + 34, x4:ob2.left + 160, y4: ob2.top + 34}
 
 
 
-  // console.log(obstacle1, obstacle2);
+
+  // console.log('ob1: ', obstacle1, 'ob2: ', obstacle2);
 
   // if (obstacle1.w >= hero.x && obstacle1.h >= blackOne.y);
 
-}
-
-setInterval(detectCollision, 100)
+// }
+//
+// setInterval(detectCollision, 100)
 
 
 // $('.obstacle').animate({bottom: "+=1000"}, 5000)
